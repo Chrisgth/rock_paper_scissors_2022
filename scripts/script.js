89942db10edd9ee11f127793db0893b7ef69c71d
chrisgth <ksemkus00@gmail.com>
2022-03-12 13:27:47 +0200

    Created a function to play a single round of RPS.

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,20 @@ function computerPlay(){
     }
     return handSign;
 }
+// Write a function that plays a single round of Rock Paper Scissors. 
+// The function should take two parameters - the playerSelection and computerSelection - and then
+// return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// Make your function’s playerSelection parameter case-insensitive 
+// (so users can input rock, ROCK, RocK or any other variation).
+
+function playSingleRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    if (playerSelection === 'Rock' && computerSelection === 'Paper'||
+    playerSelection === 'Paper' && computerSelection === 'Scissors'||
+    playerSelection === 'Scissors' && computerSelection === 'Rock'){
+        return("You lose! " + computerSelection + " beats " + playerSelection + ".");
+    } else {
+        return("You win! Congratulations!");
+    }
+}
