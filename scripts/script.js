50ce9640af54f3d0b40bc883550f0a096513392d
chrisgth <ksemkus00@gmail.com>
2022-03-12 13:50:41 +0200

    Created a function that plays 5 rounds

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,13 @@ function computerPlay(){
     let x;
     let handSign;
     x = Math.floor((Math.random() * 3) +1);
-    console.log(x);
 // Can be replaced with switch
     if (x===1){
-        handSign = "Rock";
+        handSign = 'Rock';
     } else if (x===2){
-        handSign = "Paper";
+        handSign = 'Paper';
     } else {
-        handSign = "Scissors";
+        handSign = 'Scissors';
     }
     return handSign;
 }
@@ -21,13 +20,27 @@ function computerPlay(){
 // (so users can input rock, ROCK, RocK or any other variation).
 
 function playSingleRound(playerSelection, computerSelection){
+    computerSelection = computerPlay();
     playerSelection = playerSelection.toLowerCase();
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
     if (playerSelection === 'Rock' && computerSelection === 'Paper'||
     playerSelection === 'Paper' && computerSelection === 'Scissors'||
     playerSelection === 'Scissors' && computerSelection === 'Rock'){
         return("You lose! " + computerSelection + " beats " + playerSelection + ".");
-    } else {
+    } else if (playerSelection===computerSelection){
+        return("Draw!");
+    } else{
         return("You win! Congratulations!");
+    }
+}
+
+// Write a NEW function called game(). 
+// Call the playRound function inside of this one to play a 5 round game
+// that keeps score and reports a winner or loser at the end.
+
+function game(){
+    for (let i = 0; i < 5; i++){
+        playerSelection = prompt("Please enter your selection")
+        console.log(playSingleRound(playerSelection));
     }
 }
